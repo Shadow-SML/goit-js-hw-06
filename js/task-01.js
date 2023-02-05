20f97aa803cf66e909038228af869b1d6f05a7d7
Taras <112703291+Shadow-SML@users.noreply.github.com>
2023-02-05 21:30:05 +0100

    task 1

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const categories = document.querySelector("#categories");
+const items = categories.querySelectorAll(".item");
+
+console.log(`Number of categories: ${items.length}`);
+
+items.forEach((item) => {
+  const header = item.querySelector("h2").textContent;
+  const elements = item.querySelectorAll("li").length;
+
+  console.log(`Category: ${header}`);
+  console.log(`Elements: ${elements}`);
+});
