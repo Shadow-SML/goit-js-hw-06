585a4102831999c0c4af42211bd9b578e04f4548
Taras <112703291+Shadow-SML@users.noreply.github.com>
2023-02-07 15:12:20 +0100

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,39 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const controls = document.querySelector("#controls");
+const boxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  boxes.innerHTML = "";
+  let width = 30;
+  let height = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.width = `${width}px`;
+    div.style.height = `${height}px`;
+    div.style.backgroundColor = getRandomHexColor();
+
+    boxes.appendChild(div);
+
+    width += 10;
+    height += 10;
+  }
 }
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+controls.addEventListener("click", (e) => {
+  if (e.target.matches("[data-create]")) {
+    const amount = controls.querySelector("input").value;
+    createBoxes(amount);
+  } else if (e.target.matches("[data-destroy]")) {
+    destroyBoxes();
+  }
+});
