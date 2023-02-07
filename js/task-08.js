f1ce8f2ee4fa457df9f7f1103cfd8215d5c4cd4c
Taras <112703291+Shadow-SML@users.noreply.github.com>
2023-02-07 15:02:25 +0100

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (!email || !password) {
+    alert("All fields are required.");
+    return;
+  }
+
+  const data = { email, password };
+  console.log(data);
+  form.reset();
+});
