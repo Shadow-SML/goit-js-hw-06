56b525ce7e5ac766e30376480899403c27b3b15d
Taras <112703291+Shadow-SML@users.noreply.github.com>
2023-02-09 00:35:10 +0100

    correct task 5

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 input.addEventListener("input", function () {
-  if (input.value === "") {
+  if (input.value.trim() === "") {
     output.textContent = "Anonymous";
   } else {
     output.textContent = input.value;
